9293b905be8acb129d8bd2bb2f7a165a96c0bc9e
Nick Hulea <naeluh@gmail.com>
2018-08-17 15:59:08 -0700

    more layout modifications

--- html/pandoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-current_time=$(date "+%Y-%m-%d-%H_%M_%S")
-
-echo "running pandoc ...";
-
-pandoc -f html-native_divs-native_spans-raw_html -t epub-auto_identifiers epub.xhtml -o $current_time.epub --epub-metadata=metadata.yml --epub-cover-image=../static/img/promo_image.png --template=template.xhtml  --standalone --css=../site/css/epub.css
-
-echo "done";
